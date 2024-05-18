af6ff17d29f3609f73f6bf4636a154a5356a3cd6
Binus <119787571+B1nus@users.noreply.github.com>
2024-05-18 12:58:03 +0200

    Kamera höjd

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,17 +4,24 @@ extends Camera2D
 
 @export var snap_x = 500.0
 @export var x_switch = 750
+@export var y_limit = 200
 @export_range(0.0, 1.0) var snap_speed := 1.0
 @export var dash = 100.0
 
+var target := global_position
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var x = 0.0
 	if abs(player.global_position.x) < x_switch:
-		x = 0.0
+		target.x = 0.0
 	elif player.global_position.x < 0:
-		x = - snap_x
+		target.x = - snap_x
 	else:
-		x = snap_x
+		target.x = snap_x
+	print(player.global_position.y - global_position.y)
+	if player.global_position.y - global_position.y < - y_limit:
+		target.y = player.global_position.y
+	if player.global_position.y - global_position.y > y_limit:
+		target.y = player.global_position.y
 	
-	global_position.x = lerp(global_position.x, x, snap_speed)
+	global_position = lerp(global_position, target, snap_speed)
