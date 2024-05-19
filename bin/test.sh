11a02d0c3a8608a4a13d00809888e84ae32c6ee7
meatball <69751659+meatball133@users.noreply.github.com>
2024-05-19 21:48:19 +0200

    Fix export

--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,3 @@
 wget "https://github.com/godotengine/godot-builds/releases/download/4.3-dev6/Godot_v4.3-dev6_mono_linux_x86_64.zip"
 unzip Godot_v4.3-dev6_mono_linux_x86_64.zip
-./Godot_v4.3-dev6_mono_linux_x86_64/Godot_v4.3-dev6_mono_linux.x86_64 --headless --export-release 1 ./export
+./Godot_v4.3-dev6_mono_linux_x86_64/Godot_v4.3-dev6_mono_linux.x86_64 --headless --export-release Linux ./export
