63af950a71b40ba8e4f2645b761a27f3a982e89f
meatball <69751659+meatball133@users.noreply.github.com>
2024-05-19 22:05:58 +0200

    test

--- bin/test.sh
+++ bin/test.sh
@@ -3,6 +3,6 @@ unzip Godot_v4.3-dev6_mono_linux_x86_64.zip
 wget "https://github.com/godotengine/godot-builds/releases/download/4.3-dev6/Godot_v4.3-dev6_mono_export_templates.tpz"
 unzip Godot_v4.3-dev6_mono_export_templates.tpz
 mkdir -p "/home/runner/.local/share/godot/export_templates/4.3.dev6.mono/"
-mv templates /home/runner/.local/share/godot/export_templates/4.3.dev6.mono/
+mv templates/* /home/runner/.local/share/godot/export_templates/4.3.dev6.mono/
 
 ./Godot_v4.3-dev6_mono_linux_x86_64/Godot_v4.3-dev6_mono_linux.x86_64 --headless --export-release Linux ./export
