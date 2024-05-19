9d122aff07de55d9ee4a2cf570b6c80a36b415bd
Felix-Andersson <hardaskott1337@gmail.com>
2024-05-19 16:09:47 +0200

    sprites

--- scripts/Larv.cs
+++ scripts/Larv.cs
@@ -3,64 +3,64 @@ using System;
 
 public partial class Larv : CharacterBody2D
 {
-    [Export(PropertyHint.Range, "1, 100, 1")] public float Speed = 10;
-    [Export] public Path2D Path;
-    [Export] public Sprite2D Sprite;
-    [Export] public NavigationAgent2D NavigationAgent;
-    [Export] public Area2D HurtBox;
-    [Export] public AnimationPlayer AnimationPlayer;
+	[Export(PropertyHint.Range, "1, 100, 1")] public float Speed = 10;
+	[Export] public Path2D Path;
+	[Export] public AnimatedSprite2D Sprite;
+	[Export] public NavigationAgent2D NavigationAgent;
+	[Export] public Area2D HurtBox;
+	[Export] public AnimationPlayer AnimationPlayer;
 
-    public int pointIndex = 1;
-    public Vector2 destination;
+	public int pointIndex = 1;
+	public Vector2 destination;
 
-    public override void _Ready()
-    {
-        destination = Path.Curve.GetPointPosition(pointIndex) + Path.GlobalPosition;
-        NavigationAgent.TargetPosition = destination;
-        NavigationAgent.NavigationFinished += HandleNavigationFinished;
-        HurtBox.AreaEntered  += HandleHurtBoxOverlap;
-        AnimationPlayer.AnimationFinished += HandleAnimationFinished;
-    }
+	public override void _Ready()
+	{
+		destination = Path.Curve.GetPointPosition(pointIndex) + Path.GlobalPosition;
+		NavigationAgent.TargetPosition = destination;
+		NavigationAgent.NavigationFinished += HandleNavigationFinished;
+		HurtBox.AreaEntered  += HandleHurtBoxOverlap;
+		AnimationPlayer.AnimationFinished += HandleAnimationFinished;
+	}
 
-    private void HandleAnimationFinished(StringName animName)
-    {
-        if (animName == "Death")
-        {
-            QueueFree();
-        }
-    }
-
-
-    private void HandleHurtBoxOverlap(Area2D area)
-    {
-        GD.Print("Larv hit by " + area.Name);
-        SetPhysicsProcess(false);
-        
-        AnimationPlayer.Play("Death");
-    }
+	private void HandleAnimationFinished(StringName animName)
+	{
+		if (animName == "Death")
+		{
+			QueueFree();
+		}
+	}
 
 
-    private void HandleNavigationFinished()
-    {
-        pointIndex = Mathf.Wrap(pointIndex + 1, 0 , Path.Curve.PointCount);
-        destination = Path.Curve.GetPointPosition(pointIndex) + Path.GlobalPosition;
-        NavigationAgent.TargetPosition = destination;
-    }
+	private void HandleHurtBoxOverlap(Area2D area)
+	{
+		GD.Print("Larv hit by " + area.Name);
+		SetPhysicsProcess(false);
+		
+		AnimationPlayer.Play("Death");
+	}
 
 
-    public override void _PhysicsProcess(double delta)
-    {
-        NavigationAgent.GetNextPathPosition();
-        Flip();
-        Velocity = GlobalPosition.DirectionTo(destination) * Speed;
-        MoveAndSlide();
-    }
+	private void HandleNavigationFinished()
+	{
+		pointIndex = Mathf.Wrap(pointIndex + 1, 0 , Path.Curve.PointCount);
+		destination = Path.Curve.GetPointPosition(pointIndex) + Path.GlobalPosition;
+		NavigationAgent.TargetPosition = destination;
+	}
 
-    public void Flip()
-    {
-        if (Velocity.X == 0) { return; }
-        
-        bool isMovingLeft = Velocity.X < 0;
-        Sprite.FlipH = isMovingLeft;
-    }
+
+	public override void _PhysicsProcess(double delta)
+	{
+		NavigationAgent.GetNextPathPosition();
+		Flip();
+		Velocity = GlobalPosition.DirectionTo(destination) * Speed;
+		MoveAndSlide();
+	}
+
+	public void Flip()
+	{
+		if (Velocity.X == 0) { return; }
+		
+		bool isMovingLeft = Velocity.X > 0;
+		Sprite.FlipH = isMovingLeft;
+	}
 }
