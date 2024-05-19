0cb1a63d7833ecfe0b966e8f518b176bbf7b59ca
meatball <69751659+meatball133@users.noreply.github.com>
2024-05-19 22:00:34 +0200

    Fix again

--- bin/test.sh
+++ bin/test.sh
@@ -2,7 +2,7 @@ wget "https://github.com/godotengine/godot-builds/releases/download/4.3-dev6/God
 unzip Godot_v4.3-dev6_mono_linux_x86_64.zip
 wget "https://github.com/godotengine/godot-builds/releases/download/4.3-dev6/Godot_v4.3-dev6_mono_export_templates.tpz"
 unzip Godot_v4.3-dev6_mono_export_templates.tpz
-mkdir "/home/runner/.local/share/godot/export_templates/4.3.dev6.mono/"
+mkdir "/godot/export_templates/4.3.dev6.mono/"
 mv templates /home/runner/.local/share/godot/export_templates/4.3.dev6.mono/
 
 ./Godot_v4.3-dev6_mono_linux_x86_64/Godot_v4.3-dev6_mono_linux.x86_64 --headless --export-release Linux ./export
