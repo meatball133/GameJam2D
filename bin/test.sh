3a94ae3dfef7830043838cee85475b01661e141a
meatball <69751659+meatball133@users.noreply.github.com>
2024-05-19 21:45:09 +0200

    Fix typo

--- bin/test.sh
+++ bin/test.sh
@@ -1,5 +1,5 @@
 wget "https://github.com/godotengine/godot-builds/releases/download/4.3-dev6/Godot_v4.3-dev6_mono_linux_x86_64.zip"
 unzip Godot_v4.3-dev6_mono_linux_x86_64.zip
-mv Godot_v4.3-dev6_mono_linux_x86_64/Godot_v4.3-dev6_mono_linux.x86_64 ./godo
+mv Godot_v4.3-dev6_mono_linux_x86_64/Godot_v4.3-dev6_mono_linux.x86_64 ./godot
 ./godot --build-solutions --headless
 ./godot --headless --export-release 1 ./export
