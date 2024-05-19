10f8abb32b1342c9791557da01b6e5d307541462
meatball <69751659+meatball133@users.noreply.github.com>
2024-05-19 12:47:57 +0200

    Lägg till start ui (#33)

---
+++ scripts/ui/Ui.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Ui : Control
+{
+
+    [Export] Button StartButton;
+    [Export] Container startPanel;
+
+    public override void _Ready()
+    {
+        GD.Print("Ui Ready");
+        GetTree().Paused = true;
+        StartButton.Pressed += HandleStartButtonPressed;
+    }
+
+    private void HandleStartButtonPressed()
+    {
+        GD.Print("Start Button Pressed");
+        GetTree().Paused = false;
+        startPanel.Visible = false;
+    }
+
+}
